d460f18391351cd72658936657884f428d04a0b3
罗坤 <1498710037@qq.com>
2021-03-20 14:24:52 +0800

    [pref]添加"自动创建数据库表"脚本

--- server/db.js
+++ server/db.js
@@ -1,10 +1,29 @@
+var MySQL = require('mysql');
+var $sql = require('./sqlMap');
+
 //数据库连接配置
+var create_mysql={host:'localhost', user:'root', password:'123456',};
+var mysql = {...create_mysql, database:'vue_node_mysql_learning', port: '3306'};
+
+process.on('uncaughtException', function(err){
+  // 如果数据库vue_node_mysql_learning不存在时，创建数据库
+  if(err.errno === 1049){
+    var conn = MySQL.createConnection(create_mysql);
+    MySQL.createConnection(mysql).query($sql.base.create,function(err, result){
+      console.error('创建数据库',err,result);
+      conn.query($sql.user.create_user,'',function(err, result){
+        if(!err){
+          conn.query($sql.user.add,["admin","123456"], function(err, result){
+          })
+        }
+      });
+
+    })
+
+
+  }
+});
+
 module.exports = {
-	mysql: {
-		host:'localhost',
-		user:'root',
-		password:'123456',
-		database:'test',
-		port: '3306'
-	}
-}
+  mysql
+};
--- server/sqlMap.js
+++ server/sqlMap.js
@@ -1,11 +1,22 @@
 // sql语句
 var sqlMap = {
+  // 自动创建数据表
+  base: {
+    create: 'create database vue_node_mysql_learning'
+  },
 	// 用户
 	user: {
 		add: 'insert into user( username , password) values ( ?, ?)',
 		select_name: 'SELECT * from user where username = ?',
 		select_password : 'SELECT * from user where password = ?',
-    update_password : 'update user set password = ? where id = ?'
+    update_password : 'update user set password = ? where id = ?',
+    //脚步创建数据库表
+    create_user:`CREATE TABLE user(
+                id INT NOT NULL AUTO_INCREMENT,
+                username VARCHAR(100) NOT NULL,
+                password VARCHAR(40) NOT NULL,
+                PRIMARY KEY ( id )
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
 	}
-}
+};
 module.exports = sqlMap;
